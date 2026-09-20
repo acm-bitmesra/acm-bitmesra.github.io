943bb368113ffdc8406c8e0e21863f7c98c1758f
Shaswat Raj <shaswatraj3@gmail.com>
2026-09-21 03:49:50 +0530

    fix: use root GitHub Pages URL

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/acm-bitmesra.github.io',
-  assetPrefix: '/acm-bitmesra.github.io/',
   images: { unoptimized: true },
   trailingSlash: true,
   poweredByHeader: false,
