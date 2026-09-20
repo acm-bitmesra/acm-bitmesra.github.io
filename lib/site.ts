1aaff94a894d0a847011e48d27d575d526c8c9ff
Shaswat Raj <shaswatraj3@gmail.com>
2026-09-21 03:50:33 +0530

    fix: point production URLs to ACM BIT Mesra domain

--- lib/site.ts
+++ lib/site.ts
@@ -1,6 +1,8 @@
-export const SITE_URL = 'https://sh20raj.github.io/acm-bitmesra.github.io';
+export const SITE_URL = 'https://acm-bitmesra.github.io';
 export const SITE_NAME = 'ACM BIT Mesra';
 export const SITE_DESCRIPTION = 'ACM BIT Mesra is a student-led technology community building useful software, exploring research, contributing to open source, and creating technology that matters.';
-export const BASE_PATH = '/acm-bitmesra.github.io';
+export const GITHUB_REPO = 'https://github.com/acm-bitmesra/acm-bitmesra.github.io';
+export const GITHUB_ISSUES = GITHUB_REPO + '/issues';
+export const GITHUB_NEW_ISSUE = GITHUB_REPO + '/issues/new';
 
-export const withBase = (path: string) => `${BASE_PATH}${path === '/' ? '' : path}`;
+export const withBase = (path: string) => path || '/';
